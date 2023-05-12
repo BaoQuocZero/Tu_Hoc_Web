d2313cebeabd8a7939caa20141a8e907f5e837c4
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-05-12 12:21:49 +0700

    up

---
+++ Thiết kế web/F8 Java/main.js
@@ -0,0 +1,8 @@
+//Các câu lệnh không cần thiết phải có dấu ; vì xuống dòng sẽ tương đương với dấu ;
+
+// alert('Hi')
+// var fullName=('Nguyễn Lâm Quốc Bảo')
+// var age=20
+// alert(fullName)
+// alert(age)
+
