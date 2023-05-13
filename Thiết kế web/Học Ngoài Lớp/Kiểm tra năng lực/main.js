5be6288d436aa8b22c6874baffa04c4994730da9
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2023-05-13 16:51:30 +0700

    up

--- Thiết kế web/Học Ngoài Lớp/Kiểm tra năng lực/main.js
+++ Thiết kế web/Học Ngoài Lớp/Kiểm tra năng lực/main.js
@@ -4,13 +4,12 @@ var Phone
 var Email
 var Address
 
-var modal = document.getElementById('MUTOSI');
-
 function MUTOSI() {
-    myModa
+    var mua = document.getElementById(mua)
     product = ('Nồi cơm điện MUTOSI')
-    Name = document.getElementById(Name).Value
-    Phone = document.getElementById(Phone).Value
-    Email = document.getElementById(Email).Value
-    Address = document.getElementById(Address).Value
+    Name = document.getElementById(Name)
+    Phone = document.getElementById(Phone)
+    Email = document.getElementById(Email)
+    Address = document.getElementById(Address)
+    mua.style.display="block"
 }
